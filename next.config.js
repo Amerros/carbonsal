62ab452f794a1c2c8c79974c611a9063469f9233
Amerros <a.merrout2000@gmail.com>
2025-06-11 19:36:38 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: [],
+  },
+  env: {
+    STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  }
+}
+
+module.exports = nextConfig
