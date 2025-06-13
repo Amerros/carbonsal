d3a300e602ba017fb1f209e89998646ab9495aa6
Amerros <a.merrout2000@gmail.com>
2025-06-13 22:50:15 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { AuthProvider } from '../lib/AuthContext'
+import { Toaster } from 'react-hot-toast'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,9 +25,22 @@ export default function RootLayout({ children }) {
         <meta name="theme-color" content="#22c55e" />
       </head>
       <body className={`${inter.className} antialiased`}>
-        <div id="root">
-          {children}
-        </div>
+        <AuthProvider>
+          <div id="root">
+            {children}
+          </div>
+          <Toaster 
+            position="top-right"
+            toastOptions={{
+              duration: 4000,
+              style: {
+                background: 'rgba(0, 0, 0, 0.8)',
+                color: '#fff',
+                border: '1px solid rgba(34, 197, 94, 0.3)',
+              },
+            }}
+          />
+        </AuthProvider>
       </body>
     </html>
   )
